7d151966e9d46ec86bd6fa85b05d33f878c0045b
Aine Kearns <aine.kearns@hotmail.ca>
2018-01-14 11:10:10 -0800

    not intertwined

--- shaire-your-wedding/src/App.js
+++ shaire-your-wedding/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import flowers from './TopRightCorner.jpg';
+import flowers from './TopRightCorner.png';
+import bottomflowers from './BottomLeft.png';
 import backdrop from './old-wood-board-texture.jpg'
 import './App.css';
 import Overlaymap from './Overlaymap.js';
@@ -9,12 +10,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <img align="right" src={flowers} className="App-flowers" alt="Flowers" />
         <header className="App-header">
-          <img align="right" src={flowers} className="App-flowers" alt="Flowers" />
           <h1 align="left" className="App-title">Shaire Our Wedding!</h1>
         </header>
         <p className="App-intro">
-            <div className="App-backdrop" align="center">
+            <div className="App-map" align="center">
                 <div className="App-map">
                   <Overlaymap />
                 </div>
